65f45b09ad9b6fd97876e3c1266a708d18a1b6c0
AlvaradoM <mauriciomj11@gmail.com>
2020-03-22 15:21:46 -0700

    devbrannch

--- logpuzzle.py
+++ logpuzzle.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python2
 """
 Logpuzzle exercise
-
 Copyright 2010 Google Inc.
 Licensed under the Apache License, Version 2.0
 http://www.apache.org/licenses/LICENSE-2.0
-
 Google's Python Class
 http://code.google.com/edu/languages/google-python-class/
-
 Given an apache logfile, find the puzzle urls and download the images.
-
 Here's what a puzzle url looks like:
-10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
-
-"""
+10.254.254.28 - - [06/Aug/2007:00:13:48 -0700]
+"GET /~foo/puzzle-bar-aaab.jpg
+HTTP/1.0" 302 528 "-"
+"Mozilla/5.0 (Windows; U;
+Windows NT 5.1; en-US; rv:1.8.1.6)
+Gecko/20070725
+Firefox/2.0.0.6"""
 
 import os
 import re
@@ -28,8 +28,15 @@ def read_urls(filename):
     extracting the hostname from the filename itself.
     Screens out duplicate urls and returns the urls sorted into
     increasing order."""
-    # +++your code here+++
-    pass
+    listed_files = []
+    with open(filename) as f:
+        for line in f:
+            pix = re.findall(r"\S*/puzzle\S*.jpg", line)
+            if pix and pix[0] not in listed_files:
+                listed_files.append(pix[0])
+    listed_files = list(set(listed_files))
+    listed_files.sort(key=lambda item: re.search(r'\w[^-]*$', item).group(0))
+    return listed_files
 
 
 def download_images(img_urls, dest_dir):
@@ -40,15 +47,32 @@ def download_images(img_urls, dest_dir):
     with an img tag to show each local image file.
     Creates the directory if necessary.
     """
-    # +++your code here+++
-    pass
+    if not os.path.exists(str(dest_dir)):
+        os.mkdir(dest_dir)
+    with open(str(dest_dir) + '/index.html', 'w') as index_html:
+        for i, url in enumerate(img_urls):
+            urllib.urlretrieve("http://code.google.com"
+                               + url,
+                               filename=str(dest_dir)
+                               + '/'
+                               + str(i)
+                               + ".jpg")
+            index_html.write("<img src=" + '"'
+                             + str(i)
+                             + ".jpg"
+                             + '">')
 
 
 def create_parser():
     """Create an argument parser object"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--todir',  help='destination directory for downloaded images')
-    parser.add_argument('logfile', help='apache logfile to extract urls from')
+    parser.add_argument(
+        '-d',
+        '--todir',
+        help='destination directory for downloaded images')
+    parser.add_argument(
+        'logfile',
+        help='apache logfile to extract urls from')
 
     return parser
 
